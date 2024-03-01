4727fdc282de40b0607271a6e9b12fe6207edd16
It22899224J <it22899224@my.sliit.lk>
2024-03-01 11:55:45 +0530

    initial

---
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/HarvestMasterApplication.java
@@ -0,0 +1,13 @@
+package com.Backend.HarvestMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvestMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarvestMasterApplication.class, args);
+	}
+
+}
---
+++ HarvestMaster/src/test/java/com/Backend/HarvestMaster/HarvestMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backend.HarvestMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvestMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
