e0ab3c8aa27b2ae2f435aca16f7239aad75b6deb
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-03-05 00:39:20 +0530

    Buyer model implemented

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/LogisticHandler/Model/Buyer.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/LogisticHandler/Model/Buyer.java
@@ -1,7 +1,102 @@
 package com.Backend.HarvestMaster.LogisticHandler.Model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Buyer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cus_id;
+    private String cus_name;
+    private int contactNo;
+    private String deliver_address;
+    private String driver_name;
+    private String driver_id;
+    private String vehicle_number;
+    private String delivery_date;
+    private double Qty;
+    private String paddy_type;
+
+    public int getCus_id() {
+        return cus_id;
+    }
+
+    public void setCus_id(int cus_id) {
+        this.cus_id = cus_id;
+    }
+
+    public String getCus_name() {
+        return cus_name;
+    }
+
+    public void setCus_name(String cus_name) {
+        this.cus_name = cus_name;
+    }
+
+    public int getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(int contactNo) {
+        this.contactNo = contactNo;
+    }
+
+    public String getDeliver_address() {
+        return deliver_address;
+    }
+
+    public void setDeliver_address(String deliver_address) {
+        this.deliver_address = deliver_address;
+    }
+
+    public String getDriver_name() {
+        return driver_name;
+    }
+
+    public void setDriver_name(String driver_name) {
+        this.driver_name = driver_name;
+    }
+
+    public String getDriver_id() {
+        return driver_id;
+    }
+
+    public void setDriver_id(String driver_id) {
+        this.driver_id = driver_id;
+    }
+
+    public String getVehicle_number() {
+        return vehicle_number;
+    }
+
+    public void setVehicle_number(String vehicle_number) {
+        this.vehicle_number = vehicle_number;
+    }
+
+    public String getDelivery_date() {
+        return delivery_date;
+    }
+
+    public void setDelivery_date(String delivery_date) {
+        this.delivery_date = delivery_date;
+    }
+
+    public double getQty() {
+        return Qty;
+    }
+
+    public void setQty(double qty) {
+        Qty = qty;
+    }
+
+    public String getPaddy_type() {
+        return paddy_type;
+    }
+
+    public void setPaddy_type(String paddy_type) {
+        this.paddy_type = paddy_type;
+    }
 }
