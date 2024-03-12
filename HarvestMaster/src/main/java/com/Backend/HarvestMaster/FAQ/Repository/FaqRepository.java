a4bee08c7d20bb3c8962dd6160f33f0c908701b2
It22899224J <it22899224@my.sliit.lk>
2024-03-12 12:11:41 +0530

    FAQ model

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/FAQ/Repository/FaqRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Backend.HarvestMaster.FAQ.Repository;
-
-import com.Backend.HarvestMaster.FAQ.Model.FAQ;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FaqRepository extends JpaRepository<FAQ,Integer> {
-}
