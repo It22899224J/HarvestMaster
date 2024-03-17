5c34f393c449dbea57e17d5c9f1b830c9587e70c
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-03-17 16:32:13 +0530

    Order model updated

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/Order/Model/Order.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/Order/Model/Order.java
@@ -4,12 +4,17 @@ import com.Backend.HarvestMaster.Inventory.Model.Inventory;
 import com.Backend.HarvestMaster.LogisticHandler.Model.Buyer;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.annotation.processing.Generated;
 import java.util.Date;
 
+
 @Entity
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "Order")
@@ -17,6 +22,7 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long order_id;
+
 
     @ManyToOne
     @JoinColumn(name = "cus_id")
