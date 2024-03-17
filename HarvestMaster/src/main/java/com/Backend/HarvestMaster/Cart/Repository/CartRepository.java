ee26878ec487741f5fe77cfe963762941eca1caf
Dushan Dhananjaya <dushandhananjaya01@gmail.com>
2024-03-17 19:08:20 +0530

    add repo annotation

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/Cart/Repository/CartRepository.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/Cart/Repository/CartRepository.java
@@ -2,6 +2,8 @@ package com.Backend.HarvestMaster.Cart.Repository;
 
 import com.Backend.HarvestMaster.Cart.Model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartRepository extends JpaRepository<CartItem, Integer> {
 }
