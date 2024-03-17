af093c0d9a0d53d8aca37ec9f8582afe6213386d
It22899224J <it22899224@my.sliit.lk>
2024-03-17 20:59:23 +0530

    post harvest controller fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PostHarvest/Controler/PostHarvestControler.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PostHarvest/Controler/PostHarvestControler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@CrossOrigin("http://localhost:5173/")
 @RestController
 @RequestMapping("/postharvest")
 public class PostHarvestControler {
