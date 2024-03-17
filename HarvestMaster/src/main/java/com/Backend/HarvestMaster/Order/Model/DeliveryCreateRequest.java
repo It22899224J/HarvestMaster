665369c2e6b043d280f8913dbf998fdc14fc347d
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-03-17 20:42:30 +0530

    Delivery Create Request Model

---
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/Order/Model/DeliveryCreateRequest.java
@@ -0,0 +1,33 @@
+package com.Backend.HarvestMaster.Order.Model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeliveryCreateRequest {
+    @JsonProperty("order_id")
+    private int order_id;
+
+    @JsonProperty("customer_name")
+    private String customerName;
+
+    @JsonProperty("delivery_address")
+    private String deliveryAddress;
+
+    @JsonProperty("pickup_address")
+    private String pickupAddress;
+
+    @JsonProperty("delivery_date")
+    private String deliveryDate;
+
+    @JsonProperty("driver_name")
+    private String driverName;
+
+    @JsonProperty("driver_id")
+    private String driverId;
+
+    @JsonProperty("vehicle_number")
+    private String vehicleNumber;
+}
