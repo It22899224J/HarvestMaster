c840a2f7cd966eb56349569fc5f68225942230c8
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-03-17 20:22:08 +0530

    Delivery Service updated

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/Order/Service/DeliveryService.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/Order/Service/DeliveryService.java
@@ -1,7 +1,10 @@
 package com.Backend.HarvestMaster.Order.Service;
 
 
+import com.Backend.HarvestMaster.Order.Model.CommonResponse;
 import com.Backend.HarvestMaster.Order.Model.Delivery;
+import com.Backend.HarvestMaster.Order.Model.DeliveryCreateRequest;
+import com.Backend.HarvestMaster.Order.Model.DeliveryRequest;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +13,8 @@ public interface DeliveryService {
     List<Delivery>getAllDeliverySchedules();
     Optional<Delivery>getDeliveryScheduleById(Long delivery_id);
     Delivery createDeliverySchedule(Delivery delivery);
-    Delivery updateDeliverySchedule(Long delivery_id,Delivery delivery);
+    CommonResponse updateDeliverySchedule(DeliveryRequest delivery);
     void deleteDeliverySchedule(Long delivery_id);
+
+    CommonResponse createNewDelivery(DeliveryCreateRequest request);
 }
