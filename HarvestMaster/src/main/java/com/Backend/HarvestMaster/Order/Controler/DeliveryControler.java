2f125ee9b2709720ceab25b2fc0f9728dc22c2e7
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-03-18 15:15:27 +0530

    Delivery Controler updated

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/Order/Controler/DeliveryControler.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/Order/Controler/DeliveryControler.java
@@ -2,14 +2,13 @@ package com.Backend.HarvestMaster.Order.Controler;
 
 import com.Backend.HarvestMaster.Order.Model.CommonResponse;
 import com.Backend.HarvestMaster.Order.Model.DeliveryCreateRequest;
+import com.Backend.HarvestMaster.Order.Model.ManageDeliveryRequest;
+import com.Backend.HarvestMaster.Order.Model.PendingDeliveryRequest;
 import com.Backend.HarvestMaster.Order.Service.DeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/delivery/")
@@ -53,8 +52,26 @@ public class DeliveryControler {
     }*/
 
     @PostMapping("create")
-    public ResponseEntity<?> createNewDelivery(@RequestBody DeliveryCreateRequest request) {
+    public ResponseEntity<CommonResponse> createNewDelivery(@RequestBody DeliveryCreateRequest request) {
         CommonResponse response = deliveryService.createNewDelivery(request);
+        if (response.isStatus()) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("get/pending")
+    public ResponseEntity<CommonResponse> getPendingDelivery(@RequestBody PendingDeliveryRequest request) {
+        CommonResponse response = deliveryService.getPendingDelivery(request);
+        if (response.isStatus()) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("manage")
+    public ResponseEntity<CommonResponse> manageDeliveries(@RequestBody ManageDeliveryRequest request) {
+        CommonResponse response = deliveryService.manageDeliveries(request);
         if (response.isStatus()) {
             return new ResponseEntity<>(response, HttpStatus.OK);
         }
