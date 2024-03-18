2bef3cf2d16bec99f01a0dff7136745e94258f02
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-03-18 15:14:14 +0530

    Delivery Repository updated

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/Order/Repository/DeliveryRepository.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/Order/Repository/DeliveryRepository.java
@@ -4,9 +4,13 @@ import com.Backend.HarvestMaster.Order.Model.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
     Delivery findDeliveryByDeliveryId(Long deliveryId);
+
+    List<Delivery> findDeliverysByOrderStatusAndPaymentStatus(String orderStatus, String paymentStatus);
 
 
 }
