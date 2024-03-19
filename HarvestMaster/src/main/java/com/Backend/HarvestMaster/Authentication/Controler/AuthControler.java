548fc32d45ba97b7f7fc925a92e7b27a896aea32
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-19 20:48:28 +0530

    Refactor authentication controller and add login endpoint

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/Authentication/Controler/AuthControler.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/Authentication/Controler/AuthControler.java
@@ -1,74 +1,44 @@
 package com.Backend.HarvestMaster.Authentication.Controler;
 
 
-import com.Backend.HarvestMaster.Authentication.Service.AuthService;
-import com.Backend.HarvestMaster.Authentication.model.Auth;
-import com.Backend.HarvestMaster.Authentication.model.AuthResponse;
-import com.Backend.HarvestMaster.Authentication.model.UserDetails;
-import com.Backend.HarvestMaster.Farmer.Model.Farmer;
-import com.Backend.HarvestMaster.Farmer.Service.FarmerService;
+import com.Backend.HarvestMaster.Authentication.model.LoginRequest;
+import com.Backend.HarvestMaster.Authentication.model.LoginResponse;
+import com.Backend.HarvestMaster.User.Model.USER_ROLES;
+import com.Backend.HarvestMaster.User.Model.User;
+import com.Backend.HarvestMaster.User.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.NoSuchElementException;
-
 @RestController
-@CrossOrigin("http://localhost:5173/")
-@RequestMapping("/user")
+@CrossOrigin("http://localhost:5174/")
+@RequestMapping("/api/user")
 public class AuthControler {
 
-
-
     @Autowired
-    private FarmerService farmerService;
+    private UserService userService;
 
-    @Autowired
-    private AuthService authService;
-
-    //login check and jwt auth
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> check(@RequestBody Auth auth){
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        String email = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
 
-        try{
-            if (auth.getEmail() == null){
 
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
+        String token = userService.login(email, password);
+        if (token != null) {
+            // If authentication is successful, return JWT token
+            User userDetails = userService.getUserByEmail(email);
 
-            Farmer current = farmerService.authCheck(auth.getEmail());
-            BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+            USER_ROLES userRole = userService.getUserRole(email);
 
-            if (auth.getPassword() == null){
+//            Farmer farmerDetails = farmerService.getFarmerByUserId(userDetails.getId());
 
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
-
-            //password check(incoming password with hashed one )
-            if(bCryptPasswordEncoder.matches(auth.getPassword(),current.getPassword())){
-
-                 UserDetails userDetails = new UserDetails(current.getId(),current.getEmail(),current.getName());
-
-                final String jwt = authService.jwtAuth(userDetails);
-
-                AuthResponse token = new AuthResponse(current.getName(),jwt);
-
-                return new ResponseEntity<AuthResponse>(token,HttpStatus.OK);
-
-            }
-            else{
-
-                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-            }
-
+            LoginResponse response = new LoginResponse(token, userDetails, userRole);
+            return ResponseEntity.ok(response);
+        } else {
+            // If authentication fails, return unauthorized status
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+            return null;
         }
-        catch (NoSuchElementException e)
-        {
-            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
     }
-
 }
