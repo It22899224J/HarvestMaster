566f2b3980217fa4ad35e581979674262074bdae
it22343048 <162109363+ChamudiImasha@users.noreply.github.com>
2024-03-21 15:13:14 +0530

    comment codes

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/LogisticHandler/Controler/LogisticHandlerControler.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/LogisticHandler/Controler/LogisticHandlerControler.java
@@ -1,44 +1,44 @@
-package com.Backend.HarvestMaster.LogisticHandler.Controler;
-
-import com.Backend.HarvestMaster.LogisticHandler.Model.LogisticHandler;
-import com.Backend.HarvestMaster.LogisticHandler.Repository.LogisticHandlerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.NoSuchElementException;
-
-@RestController
-@RequestMapping("/logistic")
-public class LogisticHandlerControler{
-    @Autowired
-    private LogisticHandlerRepository logisticHandlerRepository;
-
-    @PostMapping("/add")
-    public ResponseEntity<LogisticHandler> newLogisticHandler(@RequestBody LogisticHandler newLogisticHandler){
-
-
-        try {
-
-
-            return new ResponseEntity<>(logisticHandlerRepository.save(newLogisticHandler), HttpStatus.OK);
-        }catch (NoSuchElementException e){
-
-            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-
-    }
-
-
-//    @GetMapping("/LogisticHandlers")
-//    List<LogisticHandler> getallusers{
-//        return LogisticHandlerRepository.findAll();
+//package com.Backend.HarvestMaster.LogisticHandler.Controler;
+//
+//import com.Backend.HarvestMaster.LogisticHandler.Model.LogisticHandler;
+//import com.Backend.HarvestMaster.LogisticHandler.Repository.LogisticHandlerRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//import java.util.NoSuchElementException;
+//
+//@RestController
+//@RequestMapping("/logistic")
+//public class LogisticHandlerControler{
+//    @Autowired
+//    private LogisticHandlerRepository logisticHandlerRepository;
+//
+//    @PostMapping("/add")
+//    public ResponseEntity<LogisticHandler> newLogisticHandler(@RequestBody LogisticHandler newLogisticHandler){
+//
+//
+//        try {
+//
+//
+//            return new ResponseEntity<>(logisticHandlerRepository.save(newLogisticHandler), HttpStatus.OK);
+//        }catch (NoSuchElementException e){
+//
+//            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//
+//
 //    }
-
-
-
-
-}
+//
+//
+////    @GetMapping("/LogisticHandlers")
+////    List<LogisticHandler> getallusers{
+////        return LogisticHandlerRepository.findAll();
+////    }
+//
+//
+//
+//
+//}
--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/LogisticHandler/Model/LogisticHandler.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/LogisticHandler/Model/LogisticHandler.java
@@ -1,15 +1,15 @@
-package com.Backend.HarvestMaster.LogisticHandler.Model;
-
-import com.Backend.HarvestMaster.User.Model.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
-
-@Entity
-@Getter
-@Setter
-public class LogisticHandler extends User {
-    @Id
-    private Integer id;
-}
+//package com.Backend.HarvestMaster.LogisticHandler.Model;
+//
+//import com.Backend.HarvestMaster.User.Model.User;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Entity
+//@Getter
+//@Setter
+//public class LogisticHandler extends User {
+//    @Id
+//    private Integer id;
+//}
