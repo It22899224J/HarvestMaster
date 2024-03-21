5fe0cdcbb4fbadb2537496a3b37a451c262e0b33
it22343048 <162109363+ChamudiImasha@users.noreply.github.com>
2024-03-21 14:53:56 +0530

    support personnel fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PostHarvest/Controler/PostHarvestControler.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PostHarvest/Controler/PostHarvestControler.java
@@ -59,7 +59,7 @@ public class PostHarvestControler {
 
         }catch (NoSuchElementException e){
 
-            return new ResponseEntity<> (HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<> (HttpStatus.NOT_FOUND);
 
         }
 
