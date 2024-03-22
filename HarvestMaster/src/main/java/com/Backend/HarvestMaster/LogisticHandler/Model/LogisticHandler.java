032351d40a936f89cc67b88288103718de9dab5f
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-22 21:43:40 +0530

    Refactor LogisticHandler model class

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/LogisticHandler/Model/LogisticHandler.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/LogisticHandler/Model/LogisticHandler.java
@@ -1,15 +1,25 @@
-//package com.Backend.HarvestMaster.LogisticHandler.Model;
-//
-//import com.Backend.HarvestMaster.User.Model.User;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Entity
-//@Getter
-//@Setter
-//public class LogisticHandler extends User {
-//    @Id
-//    private Integer id;
-//}
+package com.Backend.HarvestMaster.LogisticHandler.Model;
+
+import com.Backend.HarvestMaster.User.Model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "Logistic_Handler")
+public class LogisticHandler {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    protected String phone;
+
+    @OneToOne
+    @JoinColumn(name = "u_id")
+    private User user;
+}
