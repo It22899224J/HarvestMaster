9cda575f2b6475ebe0debbbb95d091352b9cb06e
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-22 21:44:48 +0530

    Add methods to LogisticHandlerService interface

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/LogisticHandler/Service/LogisticHandlerService.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/LogisticHandler/Service/LogisticHandlerService.java
@@ -1,4 +1,12 @@
 package com.Backend.HarvestMaster.LogisticHandler.Service;
 
+import com.Backend.HarvestMaster.LogisticHandler.Model.LogisticHandler;
+import com.Backend.HarvestMaster.LogisticHandler.Repository.LogisticHandlerRepository;
+import com.Backend.HarvestMaster.User.Model.User;
+import com.Backend.HarvestMaster.User.Repository.UserRepository;
+
 public interface LogisticHandlerService {
+    LogisticHandler createLogisticHandler(User user, LogisticHandler logisticHandler);
+
+    LogisticHandler getLogisticHandlerByEmail(String email);
 }
