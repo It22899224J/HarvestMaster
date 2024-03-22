b69d4cb7fa501084f639ab42bae9e91d7f41a774
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2024-03-22 21:44:13 +0530

    Refactor LogisticHandlerRepository interface

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/LogisticHandler/Repository/LogisticHandlerRepository.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/LogisticHandler/Repository/LogisticHandlerRepository.java
@@ -1,9 +1,12 @@
-//package com.Backend.HarvestMaster.LogisticHandler.Repository;
-//
-//import com.Backend.HarvestMaster.LogisticHandler.Model.LogisticHandler;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface LogisticHandlerRepository extends JpaRepository<LogisticHandler,Integer> {
-//}
+package com.Backend.HarvestMaster.LogisticHandler.Repository;
+
+import com.Backend.HarvestMaster.LogisticHandler.Model.LogisticHandler;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LogisticHandlerRepository extends JpaRepository<LogisticHandler,Integer> {
+    Optional<LogisticHandler> findByUserEmail(String email);
+}
