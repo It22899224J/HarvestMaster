43417709c4c2295db1f58c037749a6ccf1fca6fe
It22899224J <it22899224@my.sliit.lk>
2024-03-23 13:55:00 +0530

    community market service fix

---
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Service/CommunityMarketService.java
@@ -0,0 +1,14 @@
+package com.Backend.HarvestMaster.PaddyStocks.Service;
+
+import com.Backend.HarvestMaster.PaddyStocks.Model.PaddyStockDetails;
+
+import java.util.List;
+
+public interface CommunityMarketService {
+
+
+    public List<PaddyStockDetails> getAllStocks();
+
+
+
+}
