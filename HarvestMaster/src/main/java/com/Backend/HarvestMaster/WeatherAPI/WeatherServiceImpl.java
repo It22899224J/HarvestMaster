64e971d3547fed721ecd78d9a4b1b6fc5bd491b6
It22899224J <it22899224@my.sliit.lk>
2024-03-29 18:58:36 +0530

    weather service implementation

---
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/WeatherAPI/WeatherServiceImpl.java
@@ -0,0 +1,44 @@
+package com.Backend.HarvestMaster.WeatherAPI;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class WeatherServiceImpl implements WeatherService {
+    @Override
+
+    public WeatherModel getWeather(String zip) {
+
+        String url = "http://api.openweathermap.org/data/2.5/forecast?zip="+zip+"&appid=3cf94147a5db1997530840cdfe968ffb&units=metric";
+
+        WebClient.Builder builder = WebClient.builder();
+
+
+//Weather API invoke
+        String jsonString = builder.build()
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+
+
+
+        // Initialize ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Deserialize JSON string into WeatherResponse object
+        WeatherModel weatherresponse = null;
+        try {
+            weatherresponse = objectMapper.readValue(jsonString, WeatherModel.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return weatherresponse;
+    }
+}
