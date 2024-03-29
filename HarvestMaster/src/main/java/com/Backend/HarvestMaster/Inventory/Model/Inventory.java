f0b231ebced8b47094a22bce779236e8cc0db626
N A D NIMSARA <duvindunimsara@gmail.com>
2024-03-29 22:54:07 +0530

    inventory backend fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/Inventory/Model/Inventory.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/Inventory/Model/Inventory.java
@@ -4,8 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Inventory {
 
     @Id
@@ -16,55 +24,7 @@ public class Inventory {
     private String Description;
     private  int Packege_Type;
     private  int Price;
+    private long image;
 
-    public Inventory() {
-    }
 
-    public int getpId() {
-        return pId;
-    }
-
-    public void setpId(int pId) {
-        this.pId = pId;
-    }
-
-    public String getProduct_type() {
-        return Product_type;
-    }
-
-    public void setProduct_type(String product_type) {
-        Product_type = product_type;
-    }
-
-    public String getProduct_Name() {
-        return Product_Name;
-    }
-
-    public void setProduct_Name(String product_Name) {
-        Product_Name = product_Name;
-    }
-
-    public String getDescription() {
-        return Description;
-    }
-
-    public void setDescription(String description) {
-        Description = description;
-    }
-
-    public int getPackege_Type() {
-        return Packege_Type;
-    }
-
-    public void setPackege_Type(int packege_Type) {
-        Packege_Type = packege_Type;
-    }
-
-    public int getPrice() {
-        return Price;
-    }
-
-    public void setPrice(int price) {
-        Price = price;
-    }
 }
