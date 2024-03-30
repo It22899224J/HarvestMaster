24258ad8a02ca3956906a325506c7a20d3e50cbf
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-03-30 23:51:36 +0530

    Modify autowired using RequiredArgsConstructor annotation

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/Order/Service/DeliveryServiceImpl.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/Order/Service/DeliveryServiceImpl.java
@@ -2,37 +2,26 @@ package com.Backend.HarvestMaster.Order.Service;
 
 import com.Backend.HarvestMaster.Cart.Model.CartItem;
 import com.Backend.HarvestMaster.Cart.Repository.CartRepository;
+import com.Backend.HarvestMaster.Inventory.Repository.InventoryRepository;
 import com.Backend.HarvestMaster.Order.Model.*;
+import com.Backend.HarvestMaster.Order.Repository.DeliveryLogActivityRepository;
 import com.Backend.HarvestMaster.Order.Repository.DeliveryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class DeliveryServiceImpl implements DeliveryService {
-    @Autowired
-    private DeliveryRepository deliveryRepository;
-    @Autowired
-    private CartRepository cartRepository;
 
-    @Override
-    public List<Delivery> getAllDeliverySchedules() {
-        return deliveryRepository.findAll();
-    }
-
-    @Override
-    public Optional<Delivery> getDeliveryScheduleById(Long delivery_id) {
-        return deliveryRepository.findById(delivery_id);
-    }
-
-    @Override
-    public Delivery createDeliverySchedule(Delivery delivery) {
-        return deliveryRepository.save(delivery);
-    }
+    private final DeliveryRepository deliveryRepository;
+    private final CartRepository cartRepository;
+    private final DeliveryLogActivityRepository logActivityRepository;
+    private final InventoryRepository inventoryRepository;
 
     @Override
     public CommonResponse updateDeliverySchedule(DeliveryRequest delivery) {
@@ -42,6 +31,13 @@ public class DeliveryServiceImpl implements DeliveryService {
             deliveryData.setDeliveryAddress(delivery.getDeliveryAddress());
             deliveryData.setPickupAddress(delivery.getPickupAddress());
             deliveryRepository.save(deliveryData);
+
+            LogActivity logActivity = LogActivity.builder()
+                    .deliveryId(deliveryData)
+                    .details("Order Updated")
+                    .cartId(deliveryData.getCartId())
+                    .build();
+            logActivityRepository.save(logActivity);
 
             return CommonResponse.builder()
                     .message("Updated")
@@ -55,12 +51,34 @@ public class DeliveryServiceImpl implements DeliveryService {
         }
     }
 
+
     @Override
-    public void deleteDeliverySchedule(Long delivery_id) {
-        deliveryRepository.deleteById(delivery_id);
+    public CommonResponse markAsDelivered(DeliveryConfirmRequest delivery) {
+        Delivery deliveryData = deliveryRepository.findDeliveryByDeliveryId(delivery.getDeliveryId());
+
+        if (deliveryData != null) {
+            deliveryData.setDeliveryStatus("DELIVERED");
+            deliveryRepository.save(deliveryData);
+
+            LogActivity logActivity = LogActivity.builder()
+                    .deliveryId(deliveryData)
+                    .details("Order Delivered")
+                    .cartId(deliveryData.getCartId())
+                    .build();
+            logActivityRepository.save(logActivity);
+
+            return CommonResponse.builder()
+                    .message("Delivered")
+                    .status(true)
+                    .build();
+        } else {
+            return CommonResponse.builder()
+                    .message("Delivery not found!")
+                    .status(false)
+                    .build();
+        }
     }
 
-    //============================================
     @Override
     public CommonResponse createNewDelivery(DeliveryCreateRequest request) {
 
@@ -92,7 +110,7 @@ public class DeliveryServiceImpl implements DeliveryService {
 
     @Override
     public CommonResponse getPendingDelivery(PendingDeliveryRequest request) {
-        List<Delivery> deliveries = deliveryRepository.findDeliverysByOrderStatusAndPaymentStatus(request.getOrderStatus(), request.getPaymentStatus());
+        List<Delivery> deliveries = deliveryRepository.findDeliverysByOrderStatusAndPaymentStatusAndDeliveryStatus(request.getOrderStatus(), request.getPaymentStatus(), "PENDING");
         List<PendingDeliveryResponse> pendingDeliveries = new ArrayList<>();
 
         for (Delivery item : deliveries) {
@@ -128,7 +146,7 @@ public class DeliveryServiceImpl implements DeliveryService {
                     .build();
         }
         Delivery deliveryDetails = delivery.get();
-        deliveryDetails.setOrderStatus(request.isOrderStatus()?"APPROVED":"REJECTED");
+        deliveryDetails.setOrderStatus(request.isOrderStatus() ? "APPROVED" : "REJECTED");
         deliveryDetails.setReason(request.getReason());
 //        deliveryDetails.setReason(StringUtils.hasLength(request.getReason())?request.getReason():null);
         deliveryRepository.save(deliveryDetails);
@@ -136,6 +154,52 @@ public class DeliveryServiceImpl implements DeliveryService {
                 .status(true)
                 .message("Delivery Managed")
                 .data(null)
+                .build();
+    }
+
+    @Override
+    public CommonResponse deliveryLogActivity() {
+        ArrayList<LogActivityResponse> list = new ArrayList<>();
+
+        List<LogActivity> activities = logActivityRepository.findAll();
+
+        for (LogActivity activity : activities) {
+            list.add(
+                    LogActivityResponse.builder()
+                            .date(activity.getDate().toLocalDate().toString())
+                            .time(activity.getDate().toLocalTime().toString())
+                            .detail(activity.getDetails())
+                            .cartId(activity.getCartId())
+                            .build()
+            );
+        }
+
+
+        return CommonResponse.builder()
+                .status(true)
+                .message("Delivery Log Activities")
+                .data(list)
+                .build();
+    }
+
+    @Override
+    public CommonResponse orderTotal() {
+        long pendingCount = deliveryRepository.countByDeliveryStatus("PENDING");
+        long deliveredCount = deliveryRepository.countByDeliveryStatus("DELIVERED");
+        long inventoryCount = inventoryRepository.count();
+        System.out.println("pendingCount : " + pendingCount);
+        System.out.println("deliveredCount : " + deliveredCount);
+        System.out.println("inventory_count : " + inventoryCount);
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("pending_count", pendingCount);
+        map.put("delivered_count", deliveredCount);
+        map.put("inventory_count", inventoryCount);
+
+        return CommonResponse.builder()
+                .status(true)
+                .message("Total")
+                .data(map)
                 .build();
     }
 }
