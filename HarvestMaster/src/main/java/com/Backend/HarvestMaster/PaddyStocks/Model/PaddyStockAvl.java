4d990908908a95e2605d6ae21b9964afa1964600
It22899224J <it22899224@my.sliit.lk>
2024-04-03 21:26:20 +0530

    bid stock available

---
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Model/PaddyStockAvl.java
@@ -0,0 +1,20 @@
+package com.Backend.HarvestMaster.PaddyStocks.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+public class PaddyStockAvl {
+
+    private float price;
+
+    public PaddyStockAvl() {
+    }
+
+    public PaddyStockAvl(float price) {
+        this.price = price;
+    }
+}
