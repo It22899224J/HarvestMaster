7bbb0555e1ee405d4e446113bdb014e3792a99f7
N A D NIMSARA <duvindunimsara@gmail.com>
2024-04-03 21:36:28 +0530

    inventory DTO

---
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/Inventory/Model/InventoryDTO.java
@@ -0,0 +1,31 @@
+package com.Backend.HarvestMaster.Inventory.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class InventoryDTO {
+
+
+    private int pId;
+    private String Product_type;
+    private String Product_Name;
+    private String Description;
+    private  int Packege_Type;
+    private  double Price;
+    private String image;
+
+
+}
