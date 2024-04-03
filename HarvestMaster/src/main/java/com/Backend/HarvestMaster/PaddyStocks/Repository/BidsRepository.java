1d626740c742b8f2189423bd72900ce9909ce753
It22899224J <it22899224@my.sliit.lk>
2024-04-03 21:26:01 +0530

    bid repository

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Repository/BidsRepository.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Repository/BidsRepository.java
@@ -1,10 +1,13 @@
 package com.Backend.HarvestMaster.PaddyStocks.Repository;
 
 import com.Backend.HarvestMaster.PaddyStocks.Model.Bid;
+import com.Backend.HarvestMaster.PaddyStocks.Model.BidDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BidsRepository extends JpaRepository<Bid,Integer> {
     List<Bid> findAllBystockid(Integer stockId);
+
+    List<BidDTO> findAllbidsBystockid(Integer stockId);
 }
