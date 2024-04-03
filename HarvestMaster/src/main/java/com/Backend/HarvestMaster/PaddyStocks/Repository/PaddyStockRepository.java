ee6e3960e533c9695a11e7a61a7442559204e860
It22899224J <it22899224@my.sliit.lk>
2024-04-03 21:27:26 +0530

    paddy stock repository

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Repository/PaddyStockRepository.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Repository/PaddyStockRepository.java
@@ -1,7 +1,9 @@
 package com.Backend.HarvestMaster.PaddyStocks.Repository;
 
 import com.Backend.HarvestMaster.PaddyStocks.Model.PaddyStock;
+import com.Backend.HarvestMaster.PaddyStocks.Model.PaddyStockAvl;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Map;
@@ -10,4 +12,8 @@ public interface PaddyStockRepository extends JpaRepository<PaddyStock,Integer> 
 
 
     List<PaddyStock> findByRelatedPostHarvest_FieldId(int fieldId);
+
+
+    @Query(value = "SELECT s.price FROM paddy_stock s JOIN post_harvest p ON s.related_postharvest_id=p.field_id  WHERE  p.paddy_vareity=?1 AND p.fertilizer_type=?2 ",nativeQuery = true)
+    List<Object[]> findByVareityAndFertilizer(String vareity,String fert);
 }
