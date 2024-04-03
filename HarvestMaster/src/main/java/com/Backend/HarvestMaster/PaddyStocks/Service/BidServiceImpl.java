1979b7a889b985f85701ee4e925ee1e639083832
It22899224J <it22899224@my.sliit.lk>
2024-04-03 21:25:50 +0530

    bid service impl

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Service/BidServiceImpl.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Service/BidServiceImpl.java
@@ -1,11 +1,13 @@
 package com.Backend.HarvestMaster.PaddyStocks.Service;
 
-import com.Backend.HarvestMaster.PaddyStocks.Model.Bid;
+import com.Backend.HarvestMaster.PaddyStocks.Model.*;
 import com.Backend.HarvestMaster.PaddyStocks.Repository.BidsRepository;
+import com.Backend.HarvestMaster.PostHarvest.Model.PostHarvest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BidServiceImpl implements BidService{
@@ -29,6 +31,19 @@ public class BidServiceImpl implements BidService{
     }
 
     @Override
+    public List<BidDTO> getMarketBids(Integer stock_id) {
+
+
+
+
+
+        return bidsRepository.findAllbidsBystockid(stock_id);
+
+
+
+    }
+
+    @Override
     public Bid updateBid(Integer bid_id, Bid bid) {
 
         Bid current = bidsRepository.findById(bid_id).get();
@@ -41,4 +56,15 @@ public class BidServiceImpl implements BidService{
         bidsRepository.deleteById(bid_id);
         return true;
     }
+
+
+
+//    private BidDTO convertTobidDTO(Bid bid) {
+//        BidDTO dto = new BidDTO();
+//        dto.setPrice(bid.getPrice());
+//        dto.setDate(bid.getCreationDate());
+//
+//        return dto;
+//    }
+
 }
