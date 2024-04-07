ff6589dbfa6effeb294e41e11d63899744cfd881
N A D NIMSARA <duvindunimsara@gmail.com>
2024-04-07 21:53:03 +0530

    inventory bug fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/Inventory/Service/InventoryServiceImpl.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/Inventory/Service/InventoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.Backend.HarvestMaster.Inventory.Service;
 
 import com.Backend.HarvestMaster.Inventory.Model.Inventory;
 import com.Backend.HarvestMaster.Inventory.Model.InventoryDTO;
+import com.Backend.HarvestMaster.Inventory.Model.Update_Inventory;
 import com.Backend.HarvestMaster.Inventory.Repository.InventoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,8 @@ import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.Base64;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -51,13 +54,27 @@ public class InventoryServiceImpl implements InventoryService{
 
 
 
-    @Override
-    public Inventory updateInventory(Integer p_id, Inventory inventory) {
 
-        inventory.setPId(p_id);
-        inventoryRepository.save(inventory);
-        return inventory;
+
+
+
+    public InventoryDTO updateInventory(Integer p_id, Update_Inventory updateInventory) {
+        Optional<Inventory> optionalInventory = inventoryRepository.findById(p_id);
+        if (optionalInventory.isPresent()) {
+            Inventory inventory = optionalInventory.get();
+            // Update the inventory details with the provided DT
+            inventory.setDescription(updateInventory.getDescription());
+            inventory.setPrice(updateInventory.getPrice());
+            // Save the updated inventory
+            inventoryRepository.save(inventory);
+            // Convert the updated inventory back to DTO for response
+            return convertToDTO(inventory);
+        } else {
+            // Handle the case where inventory with the provided ID is not found
+            throw new NoSuchElementException("Inventory not found with ID: " + p_id);
+        }
     }
+
 
     private String blobConverter(Inventory inventory){
         String imageBase64 = null;
@@ -92,6 +109,16 @@ public class InventoryServiceImpl implements InventoryService{
 
         return dto;
     }
+
+//    private InventoryDTO convertToDTOForUpdate(Inventory inventory) {
+//        InventoryDTO dto = new InventoryDTO();
+//        dto.setPId(inventory.getPId()); // Set the PID
+//        dto.setProduct_Name(inventory.getProduct_Name());
+//        dto.setDescription(inventory.getDescription());
+//        dto.setPrice(inventory.getPrice());
+//        // Exclude image data if it's not needed for update
+//        return dto;
+//    }
 }
 
 
