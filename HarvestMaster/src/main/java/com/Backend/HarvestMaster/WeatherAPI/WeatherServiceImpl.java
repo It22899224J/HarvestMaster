7eb779a45be9af0a0e802e9779efefa0ce1e9a86
It22899224J <it22899224@my.sliit.lk>
2024-04-08 12:24:03 +0530

    weather service impl fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/WeatherAPI/WeatherServiceImpl.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/WeatherAPI/WeatherServiceImpl.java
@@ -41,4 +41,36 @@ public class WeatherServiceImpl implements WeatherService {
 
         return weatherresponse;
     }
+
+    @Override
+    public CurrentWeather getCurrentWeather(String zip) {
+        String url = "http://api.openweathermap.org/data/2.5/weather?zip="+zip+",lk&appid=ddb64a174007a68a4edc85f09f65f2e6&units=metric";
+
+        WebClient.Builder builder = WebClient.builder();
+
+
+//Weather API invoke
+        String jsonString = builder.build()
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+
+
+
+        // Initialize ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Deserialize JSON string into WeatherResponse object
+        CurrentWeather currentWeatherResponse = null;
+        try {
+            currentWeatherResponse = objectMapper.readValue(jsonString, CurrentWeather.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return currentWeatherResponse;
+    }
 }
