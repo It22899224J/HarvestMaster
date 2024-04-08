577fd181f41c77a9989d59e363fcef847ebaec54
It22899224J <it22899224@my.sliit.lk>
2024-04-08 12:23:49 +0530

    weather service fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/WeatherAPI/WeatherService.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/WeatherAPI/WeatherService.java
@@ -3,4 +3,6 @@ package com.Backend.HarvestMaster.WeatherAPI;
 public interface WeatherService {
 
     public WeatherModel getWeather(String zip);
+
+    public CurrentWeather getCurrentWeather(String zip);
 }
