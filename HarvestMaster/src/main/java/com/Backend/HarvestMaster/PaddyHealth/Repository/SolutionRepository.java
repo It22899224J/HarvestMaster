4b2dc961767aab19f9bc4ab5c195d3ac5e90ee85
Dulashi1 <dulashi123456@gmail.com>
2024-04-10 22:29:47 +0530

    make issue and solution together fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyHealth/Repository/SolutionRepository.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyHealth/Repository/SolutionRepository.java
@@ -2,6 +2,11 @@ package com.Backend.HarvestMaster.PaddyHealth.Repository;
 import com.Backend.HarvestMaster.PaddyHealth.Model.Solution;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface SolutionRepository extends JpaRepository<Solution, Integer> {
+
+    List<Solution> findByIssueId(int issue_id);
 }
