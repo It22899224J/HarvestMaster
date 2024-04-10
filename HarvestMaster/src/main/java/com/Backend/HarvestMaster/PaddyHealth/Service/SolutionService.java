fb65836a4a6e26a80d72d509685f3acb881c440d
Dulashi1 <dulashi123456@gmail.com>
2024-04-10 22:42:31 +0530

    make issue and solution together fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyHealth/Service/SolutionService.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyHealth/Service/SolutionService.java
@@ -18,6 +18,7 @@ public interface SolutionService {
     // Method to update an existing solution
     Solution updateSolution(int id, Solution solution);
 
-
+    // Method to get solutions for a specific issue
+    List<Solution> getSolutionsForIssue(int issue_id);
 }
 
