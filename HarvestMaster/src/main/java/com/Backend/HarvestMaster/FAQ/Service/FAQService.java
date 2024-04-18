d229ca39ad3a99a7e73bef24451f6a760d647fe2
it22343048 <162109363+ChamudiImasha@users.noreply.github.com>
2024-04-18 11:44:34 +0530

    FAQ service fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/FAQ/Service/FAQService.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/FAQ/Service/FAQService.java
@@ -7,9 +7,9 @@ import java.util.List;
 public interface FAQService {
 
     public FAQ addFaq (FAQ faq);
-    public FAQ getFaq (int faq_id);
+    public FAQ getSelectedFaq (int faq_id);
 
-    public List<FAQ>  getAllFaq ();
+    public List<FAQ>  getAllFaqs ();
     public boolean removeFaq (int faq_id);
 
 
