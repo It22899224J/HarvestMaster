b9c60e6da075e09faa3318ed22ac102bcc71dade
It22899224J <it22899224@my.sliit.lk>
2024-04-18 11:20:25 +0530

    paddy stock view fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Model/PaddyStockViewDTO.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Model/PaddyStockViewDTO.java
@@ -1,10 +1,8 @@
 package com.Backend.HarvestMaster.PaddyStocks.Model;
 
 import com.Backend.HarvestMaster.PostHarvest.Model.PostHarvest;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import com.Backend.HarvestMaster.PostHarvest.Model.PostHarvestAudit;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,9 +14,8 @@ public class PaddyStockViewDTO {
     private int ps_id;
     private String image;
     //foreign key for postharvest
-    @ManyToOne
-    @JoinColumn(name = "related_postharvest_id")
-    private PostHarvest relatedPostHarvest;
+
+    private int relatedPostHarvestaudit;
     public int amount;
     private float price;
     @Enumerated(EnumType.STRING)
