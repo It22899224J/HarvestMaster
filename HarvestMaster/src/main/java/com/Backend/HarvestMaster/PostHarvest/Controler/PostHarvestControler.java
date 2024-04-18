1c568b0e6ad35e8f336ecaf6673e459318eed524
It22899224J <it22899224@my.sliit.lk>
2024-04-18 11:21:45 +0530

    post harvest controller fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PostHarvest/Controler/PostHarvestControler.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PostHarvest/Controler/PostHarvestControler.java
@@ -1,6 +1,10 @@
 package com.Backend.HarvestMaster.PostHarvest.Controler;
 
 import com.Backend.HarvestMaster.PostHarvest.Model.PostHarvest;
+import com.Backend.HarvestMaster.PostHarvest.Model.PostHarvestAudit;
+import com.Backend.HarvestMaster.PostHarvest.Model.PostHarvestAuditProjection;
+import com.Backend.HarvestMaster.PostHarvest.Model.PostHarvestPlan;
+import com.Backend.HarvestMaster.PostHarvest.Service.PostHarvestAuditService;
 import com.Backend.HarvestMaster.PostHarvest.Service.PostHarvestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,7 +13,10 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
+
+import static java.lang.Integer.parseInt;
 
 @CrossOrigin("http://localhost:5173/")
 @RestController
@@ -20,11 +27,14 @@ public class PostHarvestControler {
     @Autowired
     private PostHarvestService postHarvestService;
 
-    @PostMapping("/add")
-    public ResponseEntity addPostHarvestPlan(@RequestBody PostHarvest postHarvest){
+    @Autowired
+    private PostHarvestAuditService postHarvestAuditService;
 
-        postHarvestService.addPostHarvestPlans(postHarvest);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @PostMapping("/add")
+    public ResponseEntity<PostHarvest> addPostHarvestPlan(@RequestBody PostHarvest postHarvest){
+
+
+        return new ResponseEntity<>( postHarvestService.addPostHarvestPlans(postHarvest),HttpStatus.OK);
     }
 
     @GetMapping("/get/{id}")
@@ -39,7 +49,7 @@ public class PostHarvestControler {
     }
 
     @GetMapping("/all/{f_id}")
-    public ResponseEntity<List<PostHarvest>> getAll(@PathVariable Integer f_id){
+    public ResponseEntity<List<PostHarvestPlan>> getAll(@PathVariable Integer f_id){
         try{
 
             return new ResponseEntity<>(postHarvestService.getPostHarvestPlans(f_id),HttpStatus.OK);
@@ -54,8 +64,8 @@ public class PostHarvestControler {
         try{
 
 
-            postHarvestService.updatePostHarvestDetails(id,postHarvest);
-            return new ResponseEntity<>(HttpStatus.OK);
+
+            return new ResponseEntity<>( postHarvestService.updatePostHarvestDetails(id,postHarvest),HttpStatus.OK);
 
         }catch (NoSuchElementException e){
 
@@ -77,6 +87,104 @@ public class PostHarvestControler {
 
     }
 
+
+    @PostMapping("/add-audit")
+    public ResponseEntity<PostHarvestAudit> addPostHarvestAudit(@RequestBody Map<String, String> requestBody){
+
+        String harvestDate = requestBody.get("harvestDate");
+        String postId = requestBody.get("postId");
+        Integer postharvest_id = parseInt(postId);
+
+        PostHarvest postHarvestplan = postHarvestService.getPostHarvestDetails(postharvest_id);
+        postHarvestplan.setHarvestDate(harvestDate);
+
+        try {
+
+            PostHarvestAudit newAudit = new PostHarvestAudit();
+            if (postHarvestplan != null) {
+
+
+
+
+                //updateing  the harvesting date
+                PostHarvest updatedPlan = postHarvestService.addPostHarvestPlans(postHarvestplan);
+
+
+            }
+            return new ResponseEntity<>(postHarvestAuditService.addAudit(postharvest_id),HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.OK);
+
+
+        }
+
+
+    }
+
+    @GetMapping("/get-audit/{id}")
+    public ResponseEntity<PostHarvestAuditProjection> getpostharvestAudit(@PathVariable Integer id){
+        try {
+            PostHarvestAuditProjection postHarvestAudit = postHarvestAuditService.getAudit(id);
+            return new ResponseEntity<>(postHarvestAudit, HttpStatus.OK);
+        }catch (NoSuchElementException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PutMapping("/update-audit/{id}")
+    public ResponseEntity<PostHarvestAuditProjection> updateAudit(@PathVariable Integer id ,  @RequestBody PostHarvestAudit updatedAudit){
+
+        try{
+
+            PostHarvestAudit existingAudit = postHarvestAuditService.getAuditData(id);
+
+            if (existingAudit == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            // Update only the specified attribute(s) and ensure non-null or non-zero values
+            if (updatedAudit.getFuel() != 0.0) {
+                existingAudit.setFuel(updatedAudit.getFuel());
+            }
+            if (updatedAudit.getNo_bags() != 0) {
+                existingAudit.setNo_bags(updatedAudit.getNo_bags());
+            }
+            if ( updatedAudit.getWeight() != 0.0) {
+                existingAudit.setWeight(updatedAudit.getWeight());
+            }
+            if (updatedAudit.getQuality_value() != 0.0) {
+                existingAudit.setQuality_value(updatedAudit.getQuality_value());
+            }
+            if (updatedAudit.getNum_harvesting() != 0) {
+                existingAudit.setNum_harvesting(updatedAudit.getNum_harvesting());
+            }
+            if (updatedAudit.getHarvesting_expense() != 0.0) {
+                existingAudit.setHarvesting_expense(updatedAudit.getHarvesting_expense());
+            }
+            if (updatedAudit.getTransport_expense() != 0.0) {
+                existingAudit.setTransport_expense(updatedAudit.getTransport_expense());
+            }
+            if (updatedAudit.getIncome() != 0.0) {
+                existingAudit.setIncome(updatedAudit.getIncome());
+            }
+
+
+
+
+
+            return new ResponseEntity<>(postHarvestAuditService.updateAudit(existingAudit),HttpStatus.OK);
+
+
+
+
+        }catch (NoSuchElementException e){
+
+            return new ResponseEntity<> (HttpStatus.NOT_FOUND);
+
+        }
+
+    }
 
 
 }
