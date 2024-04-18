cb604724f9a26e49fa539fea022a5d4debb237fb
It22899224J <it22899224@my.sliit.lk>
2024-04-18 11:20:49 +0530

    post harvest audit fix

---
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PostHarvest/Model/PostHarvestAudit.java
@@ -0,0 +1,37 @@
+package com.Backend.HarvestMaster.PostHarvest.Model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class PostHarvestAudit {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int auditId;
+
+
+
+    private int no_bags;
+    private float weight;
+    private float quality_value;
+
+    private int num_harvesting;
+
+    private float fuel;
+
+    private float harvesting_expense;
+    private float transport_expense;
+
+    private float income;
+
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "related_postharvest_Id")
+    private PostHarvest relatedpostHarvest;
+
+
+}
