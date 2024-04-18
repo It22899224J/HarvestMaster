f6401938e3b85057c3d78216f31fbfbea850ae7f
It22899224J <it22899224@my.sliit.lk>
2024-04-18 11:22:06 +0530

    post harvest plan fix

---
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PostHarvest/Model/PostHarvestPlan.java
@@ -0,0 +1,12 @@
+package com.Backend.HarvestMaster.PostHarvest.Model;
+
+import java.time.LocalDate;
+
+public interface PostHarvestPlan {
+
+    int getFieldId();
+    String getLocation();
+    LocalDate getPlantedDate();
+    String getFieldName();
+
+}
