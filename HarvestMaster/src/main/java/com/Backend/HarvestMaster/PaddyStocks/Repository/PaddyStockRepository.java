c29dbc0c4c4efe1ad618ddf30b5e1a8c5678cd55
It22899224J <it22899224@my.sliit.lk>
2024-04-20 00:22:41 +0530

    paddy stock  repository fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Repository/PaddyStockRepository.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Repository/PaddyStockRepository.java
@@ -17,6 +17,6 @@ public interface PaddyStockRepository extends JpaRepository<PaddyStock,Integer> 
     @Query(value = "SELECT s.* FROM paddy_stock s JOIN post_harvest_audit a ON s.related_postharvest_audit_id=a.audit_id  WHERE  a.audit_id=?1  ",nativeQuery = true)
     List<PaddyStock> findByRelatedPostHarvest_FieldId(int auditId);
 
-    @Query(value = "SELECT s.price FROM paddy_stock s JOIN post_harvest_audit a ON s.related_postharvest_audit_id=a.audit_id JOIN  post_harvest p  ON a.related_postharvest_id=p.field_id WHERE  p.paddy_vareity=?1 AND p.fertilizer_type=?2 ",nativeQuery = true)
-    List<Object[]> findByVareityAndFertilizer(String vareity,String fert);
+    @Query(value = "SELECT s.price As price ,s.stock_creation_date As creationDate FROM paddy_stock s JOIN post_harvest_audit a ON s.related_postharvest_audit_id=a.audit_id JOIN  post_harvest p  ON a.related_postharvest_id=p.field_id WHERE  p.paddy_vareity=?1 AND p.fertilizer_type=?2 ",nativeQuery = true)
+    List<PaddyStockAvl> findByVareityAndFertilizer(String vareity,String fert);
 }
