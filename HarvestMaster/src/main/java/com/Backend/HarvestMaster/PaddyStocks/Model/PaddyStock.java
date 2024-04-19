4eb72efe70b36b05d7ee653016ffdf4971cfdf8e
It22899224J <it22899224@my.sliit.lk>
2024-04-20 00:19:18 +0530

    paddy stock fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Model/PaddyStock.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Model/PaddyStock.java
@@ -5,8 +5,10 @@ import com.Backend.HarvestMaster.PostHarvest.Model.PostHarvestAudit;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Blob;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -22,13 +24,17 @@ public class PaddyStock {
 @Lob
     private Blob image;
     //foreign key for postharvestAudit
-    @ManyToOne(cascade = CascadeType.REMOVE)
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinColumn(name = "related_postharvest_auditId")
     private PostHarvestAudit relatedPostHarvestaudit;
     public int amount;
+
     private float price;
     @Enumerated(EnumType.STRING)
     private Status_stock status;
+    @CreationTimestamp
+    @Column(name = "stockCreationDate", updatable = false)
+    private LocalDate stockCreationDate;
 
 
 
