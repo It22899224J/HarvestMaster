18a559d6a2d4a5bcfed744e34050e37bec477a51
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-04-20 14:13:23 +0530

    pre harvest costs Repo native query

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PreHarvest/Repository/PreHarvestCostsRepository.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PreHarvest/Repository/PreHarvestCostsRepository.java
@@ -1,12 +1,14 @@
 package com.Backend.HarvestMaster.PreHarvest.Repository;
 
+import com.Backend.HarvestMaster.PreHarvest.Model.Projections.PreHarvestCostProjection;
 import com.Backend.HarvestMaster.PreHarvest.Model.PreHarvestCosts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface PreHarvestCostsRepository extends JpaRepository<PreHarvestCosts, Integer> {
-    List<PreHarvestCosts> findCostsByPreHarvestPlansFieldId(Integer fieldId);
+    @Query(value = "SELECT cost_id As costId, amount As amount, date As date, description As description, type As type, field_id As fieldId FROM pre_harvest_costs  WHERE field_id = ?1", nativeQuery = true)
+    List<PreHarvestCostProjection> findAllProjected(Integer field_id);
 }
