0150f0440c71d454986659466a623a759651daa3
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-04-20 14:11:48 +0530

    pre harvest costs

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PreHarvest/Model/PreHarvestCosts.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PreHarvest/Model/PreHarvestCosts.java
@@ -11,15 +11,17 @@ import lombok.Setter;
 public class PreHarvestCosts {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int costId;
-    private String date;
-    private String description;
-    private float amount;
+    private int costId; // Unique identifier for the cost
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "field_id")
-    @JsonIgnoreProperties("preHarvestCosts")
-    private PreHarvestPlans preHarvestPlans;
+    private String date; // Date of the cost
+    private String type; // Type of cost (e.g., labor, materials)
+    private String description; // Description of the cost
+    private float amount; // Amount of the cost
+
+    @ManyToOne(cascade={CascadeType.PERSIST,CascadeType.MERGE}) // Many costs can belong to one pre-harvest plan
+    @JoinColumn(name = "field_id") // Name of the foreign key column in the database
+    @JsonIgnoreProperties("preHarvestCosts") // Ignore the preHarvestCosts property to avoid infinite recursion
+    private PreHarvestPlans preHarvestPlans; // Pre-harvest plan to which the cost belongs
 
     public PreHarvestCosts() {
     }
