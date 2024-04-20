069c326d589c342129b58d7193caa86c46108235
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-04-20 14:18:02 +0530

    pre harvest field visits service

---
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PreHarvest/Service/PreHarvestFieldVisitsService.java
@@ -0,0 +1,14 @@
+package com.Backend.HarvestMaster.PreHarvest.Service;
+
+import com.Backend.HarvestMaster.PreHarvest.Model.PreHarvestFieldVisits;
+import com.Backend.HarvestMaster.PreHarvest.Model.Projections.PreHarvestFieldVisitProjection;
+
+import java.util.List;
+
+public interface PreHarvestFieldVisitsService {
+    PreHarvestFieldVisits addFieldVisit(PreHarvestFieldVisits preHarvestFieldVisits);
+    List<PreHarvestFieldVisitProjection> getAllFieldVisits(Integer fieldId);
+    PreHarvestFieldVisits updateFieldVisits(Integer observationId, PreHarvestFieldVisits preHarvestFieldVisits);
+    boolean deleteFieldVisit (Integer observationId);
+
+}
