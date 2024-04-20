6bd3394bcf2e3be4c58244c99cfbc371e4f9c218
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-04-20 14:14:30 +0530

    pre harvest costs service implementation update

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PreHarvest/Service/PreHarvestCostsServiceImpl.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PreHarvest/Service/PreHarvestCostsServiceImpl.java
@@ -1,14 +1,14 @@
 package com.Backend.HarvestMaster.PreHarvest.Service;
 
+import com.Backend.HarvestMaster.PreHarvest.Model.Projections.PreHarvestCostProjection;
 import com.Backend.HarvestMaster.PreHarvest.Model.PreHarvestCosts;
 import com.Backend.HarvestMaster.PreHarvest.Repository.PreHarvestCostsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+
 @Service
 public class PreHarvestCostsServiceImpl implements PreHarvestCostsService{
-
     @Autowired
     private PreHarvestCostsRepository preHarvestCostsRepository;
 
@@ -18,10 +18,9 @@ public class PreHarvestCostsServiceImpl implements PreHarvestCostsService{
     }
 
     @Override
-    public List<PreHarvestCosts> getAllCosts(Integer fieldId) {
-        return preHarvestCostsRepository.findCostsByPreHarvestPlansFieldId(fieldId);
+    public List<PreHarvestCostProjection> getAllCosts(Integer fieldId) {
+        return preHarvestCostsRepository.findAllProjected(fieldId);
     }
-
 
     @Override
     public PreHarvestCosts updateCost(Integer costId, PreHarvestCosts preHarvestCosts) {
