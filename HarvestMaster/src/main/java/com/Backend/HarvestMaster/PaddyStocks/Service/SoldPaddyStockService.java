c532fcba864170e88e2dbee72287b3539c9c0d6b
It22899224J <it22899224@my.sliit.lk>
2024-04-21 12:27:40 +0530

    sold paddystock  service

---
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaddyStocks/Service/SoldPaddyStockService.java
@@ -0,0 +1,19 @@
+package com.Backend.HarvestMaster.PaddyStocks.Service;
+
+import com.Backend.HarvestMaster.PaddyStocks.Model.Bid;
+import com.Backend.HarvestMaster.PaddyStocks.Model.SoldPaddyStock;
+import com.Backend.HarvestMaster.PaddyStocks.Model.SoldPaddyStockDTO;
+
+public interface SoldPaddyStockService {
+
+
+  public SoldPaddyStock addSoldPaddyStock(Integer stockid, Integer bidid);
+
+    public SoldPaddyStockDTO updateSoldPaddyStock(Integer id,SoldPaddyStock soldPaddyStock);
+
+    public boolean removeSoldPaddystock(Integer sStockid);
+
+    public SoldPaddyStockDTO getSoldPaddyStock(Integer stock_id);
+
+
+}
