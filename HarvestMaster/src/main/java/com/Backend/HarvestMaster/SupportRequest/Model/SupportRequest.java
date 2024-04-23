ab068e538597caf25694148b83de194b01a245e8
ChamudiImasha <162109363+ChamudiImasha@users.noreply.github.com>
2024-04-24 00:22:55 +0530

    request model

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/SupportRequest/Model/SupportRequest.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/SupportRequest/Model/SupportRequest.java
@@ -4,50 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
+@Getter
+@Setter
 
 @Entity
 public class SupportRequest {
 
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
 
     public SupportRequest() {
-    }
-
-    public int getR_Id() {
-        return r_Id;
-    }
-
-    public void setR_Id(int r_Id) {
-        this.r_Id = r_Id;
-    }
-
-    public String getIssue() {
-        return issue;
-    }
-
-    public void setIssue(String issue) {
-        this.issue = issue;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
     }
 
     @Id
@@ -60,18 +29,10 @@ public class SupportRequest {
 
     private String user_id;
 
+    private String solution;
+
     @CreationTimestamp
     private LocalDate localDate;
 
-    public void setLocalDate(LocalDate localDate) {
-        this.localDate = localDate;
-    }
 
-    public LocalDate getLocalDate() {
-        return localDate;
-    }
-
-    public String getTopic() {
-        return topic;
-    }
 }
