b95e1c92d458fe7577eef5ae0b10574205ed709e
LAHIRU <lahiruudayakumara950@gmail.com>
2024-05-13 07:38:08 +0530

    bug fix

--- HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaymentHandle/Repositiory/TransactionPaymentRepository.java
+++ HarvestMaster/src/main/java/com/Backend/HarvestMaster/PaymentHandle/Repositiory/TransactionPaymentRepository.java
@@ -1,8 +1,14 @@
 package com.Backend.HarvestMaster.PaymentHandle.Repositiory;
 
 
+import com.Backend.HarvestMaster.Order.Model.Delivery;
 import com.Backend.HarvestMaster.PaymentHandle.Model.TransactionPayment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface TransactionPaymentRepository extends JpaRepository<TransactionPayment, Long> {
+    List<TransactionPayment> findByDelivery(Delivery delivery);
 }
